02c34ceb37ff673b494e8361ec234e63457fb8a0
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2022-06-28 15:02:47 -0500

    little changes.

--- src/factions/Factions.php
+++ src/factions/Factions.php
@@ -1,15 +1,26 @@
 <?php
 namespace factions;
 
+/**
+ * Class Factions
+ * @package factions
+ */
 class Factions extends \pocketmine\plugin\PluginBase {
 
+  /** @var Factions $plugin */
   private static Factions $plugin;
-  
+
+  /**
+   * @return void
+   */
   public function onLoad(): void {
-   self::$plugin = $this;
+    self::$plugin = $this;
   }
 
+  /**
+   * @return Factions
+   */
   public static function getPlugin(): Factions {
-   return self::$plugin;
+    return self::$plugin;
   }
 }
